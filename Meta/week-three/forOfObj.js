74b0ee94fcefa2b8452df40bc084724c5ff8c0f2
codingsake <thinthazin.chris@gmail.com>
2022-09-02 01:41:00 -0400

    looping Obj

---
+++ Meta/week-three/forOfObj.js
@@ -0,0 +1,7 @@
+const car2 = {
+    speed: 200,
+    color: "red"
+}
+console.log(Object.keys(car2)); // ['speed','color']
+console.log(Object.values(car2)); //[ 200, 'red' ]
+console.log(Object.entries(car2)); // [ [ 'speed', 200 ], [ 'color', 'red' ] ]
